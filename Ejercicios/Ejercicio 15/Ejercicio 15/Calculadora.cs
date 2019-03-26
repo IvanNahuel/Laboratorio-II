d698d6033d4c5d5828f7824c7058d406b57a4166
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-03-26 20:55:07 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio 15/Ejercicio 15/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_15
+{
+    class Calculadora
+    {
+        public static float Calcular(float numero1, float numero2, string operacion)
+        {
+            float retorno = 0;
+            //hacer el caculo aca y dependiendo de el simbolo hacer un if comparando los operadores aritmeticos
+            if (operacion == "+")
+            {
+                retorno = numero1 + numero2;
+            }
+            else if (operacion == "-")
+            {
+                retorno = numero1 - numero2;
+            }
+            else if (operacion == "*")
+            {
+                retorno = numero1 * numero2;
+            }
+            else if (operacion == "/" && validar(numero2))
+            {
+                retorno = numero1 / numero2;
+            }
+
+            return retorno;
+        }
+        static bool validar(float numero)
+        {
+            bool retorno = false;
+            if (numero != 0)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio 15/Ejercicio 15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            //FALTA HACER EL INGRESO DE DATOS Y EL INGRESO DE OPERADOR ARITMETICO
+
+
+
+
+        }
+    }
+}
