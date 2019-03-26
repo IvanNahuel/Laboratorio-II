77a74ddf0e3bd554aaf80922ce97fb6dd94b4b09
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-03-26 19:28:43 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio 12/Ejercicio 12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros;
+            int acomulador = 0;
+            string respuesta;
+
+
+            do
+            {
+                Console.WriteLine("Desea ingresar un numero ? S/N");
+                respuesta = Console.ReadLine();
+                if (ValidarRespuesta.ValidaS_N(respuesta) == false)
+                {
+                    break;
+                }
+                Console.WriteLine("Ingrese el numero:");
+                numeros = int.Parse(Console.ReadLine());
+
+                acomulador += numeros;
+
+            } while (ValidarRespuesta.ValidaS_N(respuesta) == true);
+
+            Console.WriteLine("El numero acomulado es: "+acomulador);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio 12/Ejercicio 12/ValidarRespuesta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_12 {
+    class ValidarRespuesta{
+
+        public static bool ValidaS_N(string opciones)
+        {
+            bool retorno = false;
+
+            if (opciones=="S" || opciones == "s")
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
