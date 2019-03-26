3486e7442c323a0ef27747d3236dfd969f8cb5ad
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-03-26 20:53:44 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio 14/Ejercicio 14/CalculoDeArea.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_14
+{
+    public class CalculoDeArea
+    {/*
+        14. Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase(estáticos) que realicen el
+        cálculo del área que corresponda:
+        a. double CalcularCuadrado(double)
+        b. double CalcularTriangulo(double, double)
+        c. double CalcularCirculo(double)
+        El ingreso de los datos como la visualización se deberán realizar desde el método Main() */
+
+        public static double CalcularCuadrado(double lado1)
+        {
+            double l1;
+
+            l1 = ElevadoAlCuadrado(lado1);
+
+            return l1;
+        }
+        private static double ElevadoAlCuadrado(double elevacion)
+        {
+            double acomulador = 0;
+
+            acomulador = elevacion * elevacion;
+            return acomulador;
+        }
+
+        public static double CalcularAreaTriangulo(double lado1, double lado2)
+        {
+            double retorno = 0;
+
+            retorno = (lado1 * lado2) / 2;
+
+            return retorno;
+        }
+        public static double CalcularAreaCirculo(double diametro)
+        {
+            double retorno = 0;
+
+            retorno = (diametro * diametro) * 0.7854;
+
+            return retorno;
+        }
+
+
+
+
+
+    }
+}
---
+++ Ejercicios/Ejercicio 14/Ejercicio 14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio_14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1Cuadrado;
+            double AreaCuadrado;
+
+            double lado1Triangulo;
+            double lado2Triangulo;
+            double areaTriangulo;
+
+            double DiametroCirculo;
+            double areaCirculo;
+        
+            
+            Console.WriteLine("Ingrese el lado 1 del cuadrado");
+            lado1Cuadrado = int.Parse(Console.ReadLine());
+            AreaCuadrado = CalculoDeArea.CalcularCuadrado(lado1Cuadrado);
+            Console.WriteLine("el area del cuadrado es : "+AreaCuadrado);
+            
+            Console.WriteLine("Ingrese el lado 1 del triangulo Rectandulo: ");
+            lado1Triangulo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lado 2 del triangulo Rectandulo: ");
+            lado2Triangulo = int.Parse(Console.ReadLine());
+            areaTriangulo = CalculoDeArea.CalcularAreaTriangulo(lado1Triangulo, lado2Triangulo);
+            Console.WriteLine("el area del triangulo rectangulo es : " + areaTriangulo+"CM²");
+            
+            Console.WriteLine("Ingrese el diametro del circulo: ");
+            DiametroCirculo = int.Parse(Console.ReadLine());
+
+            areaCirculo = CalculoDeArea.CalcularAreaCirculo(DiametroCirculo);
+            Console.WriteLine("el diametro de un cirulo es: "+areaCirculo);
+
+            Console.ReadKey();
+        }
+    }
+}
