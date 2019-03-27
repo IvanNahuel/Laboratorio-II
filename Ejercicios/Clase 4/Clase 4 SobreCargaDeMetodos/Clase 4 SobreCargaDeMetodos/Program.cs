8bb3a6d7d9b6af4b8b3ee5d76339c016abb911a4
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-03-26 21:57:34 -0300

    Add files via upload

---
+++ Ejercicios/Clase 4/Clase 4 SobreCargaDeMetodos/Clase 4 SobreCargaDeMetodos/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Clase_4_SobreCargaDeMetodos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      long a;
+
+
+      //Ejercicio 19
+      Sumador sumador = new Sumador();
+      //Sumador sumador = new Sumador(1);  //usaremos el constructor que pide como argumento solo un entero
+
+      //Console.WriteLine(sumador.cantidadDeSumas);
+
+      Console.WriteLine(Sumador.sumar(10,10));
+      Console.WriteLine(Sumador.sumar("Hola ","Mundo"));
+
+
+
+
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Ejercicios/Clase 4/Clase 4 SobreCargaDeMetodos/Clase 4 SobreCargaDeMetodos/Sumador.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_4_SobreCargaDeMetodos
+{
+    class Sumador
+    {
+    public int cantidadDeSumas;
+
+    public Sumador()
+    {
+      cantidadDeSumas = 0;
+    }
+    public Sumador(int cantidadSumas)
+    {
+        cantidadDeSumas = cantidadSumas;
+    }
+    public static long sumar(long a, long b)
+    {
+      return a + b;
+    }
+    public static string sumar(string a,string b){
+      return a + b;
+    }
+    
+
+
+
+  }
+}
