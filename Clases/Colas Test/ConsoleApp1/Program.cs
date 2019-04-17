d826dd70c06827225620f5085492360ccfd3d7b5
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-04-16 21:38:53 -0300

    Add files via upload

---
+++ Clases/Colas Test/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Persona> personas = new Queue<Persona>();
+            personas.Enqueue(new Persona { edad = 1, nombre = "Pepe" });
+            personas.Enqueue(new Persona { edad = 10, nombre = "carlos" });
+            personas.Enqueue(new Persona { edad = 11, nombre = "Fede" });
+            personas.Enqueue(new Persona { edad = 4, nombre = "Damian" });
+
+            foreach (Persona personitas in personas)
+            {
+                Console.WriteLine(personitas.nombre);
+            }
+
+            Console.WriteLine("Siguiente.......");
+
+            System.Threading.Thread.Sleep(3000);
+
+            personas.Dequeue();
+
+            foreach (Persona personitas in personas)
+            {
+                Console.WriteLine(personitas.nombre);
+            }
+
+            Console.ReadKey();
+        }
+
+        public class Persona
+        {
+            public int edad;
+
+            public string nombre;
+        }
+
+        public void MostrarPersonas(Persona p)
+        {
+
+        }
+
+
+
+    }
+}
