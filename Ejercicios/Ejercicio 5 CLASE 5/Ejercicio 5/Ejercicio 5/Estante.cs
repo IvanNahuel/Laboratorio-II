1be6c1d8fe7cac703d69a1bd8641eb73290db5a5
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2019-04-23 16:50:33 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio 5 CLASE 5/Ejercicio 5/Ejercicio 5/Estante.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_5
+{
+    class Estante
+    {
+        Producto[] productos;
+        int ubicacionEstante;
+
+        //4) El constructor de instancia privado será el único que inicializará el array. La sobrecarga
+        //pública del constructor inicializará la
+        //ubicación del estante, recibiendo como parámetro capacidad y ubicación.Reutilizar código.
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        //5) El método público GetProductos, retornará el array de productos.
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+        //6) El método público de clase MostrarEstante, retornará una cadena con toda la información del
+        //estante, incluyendo el detalle de cada uno de sus productos.Reutilizar código.
+
+        public static string MostrarEstante(Estante e)
+        {
+            string retorno;
+            retorno = e.ubicacionEstante.ToString() + " " ;
+
+            foreach (Producto producto in e.productos)
+            {
+                if (!(e.productos is null)) //si es verdadero
+                {
+                    retorno += Producto.MostrarProducto(producto);
+                }
+            }
+            return retorno;
+        }
+
+        /*
+        Igualdad, retornará true, si es que el producto ya se encuentra en el estante, false, caso contrario.
+        +Adición, retornará true y agregará el producto si el estante posee capacidad de almacenar al menos un producto más y
+        dicho producto no se encuentra en él; false, caso contrario. Reutilizar código.
+        -Sustracción (Estante, Producto), retornará un estante sin el producto, siempre y cuando el producto se encuentre en el
+        listado. Reutilizar código
+        */
+
+        //operador MAS
+        public static bool operator +(Estante e, Producto p)
+        {
+            //true: agregara el producto, si el estante posee la capacidad de almacenar un elemento
+            //y ademas ese producto no tiene que existr.
+            if (e != p)
+            {
+                Console.WriteLine("no existe ningun producto en el estante  ");
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+           for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] == p)
+                {
+                    e.productos[i] = null;
+                    break;
+                }
+            }
+            return e;
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto producto in e.productos)
+            {
+                if (producto == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+     
+
+
+
+
+    }
+}
---
+++ Ejercicios/Ejercicio 5 CLASE 5/Ejercicio 5/Ejercicio 5/Producto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_5
+{
+    class Producto
+    {
+        string codigoDeBarras;
+        string marca;
+        float precio;
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarras = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return p.codigoDeBarras + p.marca + p.precio.ToString();
+        }
+
+        public static explicit operator string (Producto p)
+        {
+            return p.codigoDeBarras;
+        }
+        //Igualdad (Producto, Producto). Retornará true, si las marcas y códigos de 
+        //barras son iguales, false, caso contrario
+        public static bool operator ==(Producto p, Producto p2)
+        {
+            if (p is null)
+            {
+                Console.WriteLine("p es nulo y no se por que");
+            }
+
+            return p == p2;
+        }
+        public static bool operator !=(Producto p, Producto p2)
+        {
+            return !(p == p2);
+        }
+        //Igualdad (Producto, string). Retornará true, si la marca del producto coincide con la cadena
+        //pasada por parámetro, false, caso contrario.
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.marca == marca;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p.marca == marca);
+        }
+
+
+    }
+}
